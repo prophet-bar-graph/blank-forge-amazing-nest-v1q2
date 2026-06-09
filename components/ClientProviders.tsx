4d114d4e0fe78edd0117845f6ecb69eafc9b06b7
Architect by Lyzr <architect@lyzr.ai>
2026-06-09 15:10:33 +0000

    No changes to commit

--- components/ClientProviders.tsx
+++ components/ClientProviders.tsx
@@ -3,18 +3,15 @@
 import ErrorBoundary from '@/components/ErrorBoundary'
 import { AgentInterceptorProvider } from '@/components/AgentInterceptorProvider'
 import { BrandProfileProvider } from '@/components/BrandProfileProvider'
-import { SSOGuard } from '@/components/SSOGuard'
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
     <ErrorBoundary>
-      <SSOGuard>
-        <AgentInterceptorProvider>
-          <BrandProfileProvider>
-            {children}
-          </BrandProfileProvider>
-        </AgentInterceptorProvider>
-      </SSOGuard>
+      <AgentInterceptorProvider>
+        <BrandProfileProvider>
+          {children}
+        </BrandProfileProvider>
+      </AgentInterceptorProvider>
     </ErrorBoundary>
   )
 }
