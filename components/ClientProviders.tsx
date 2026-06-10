prophet-bar-graph/blank-forge-amazing-nest-v1q2

commit 4d114d4e0fe78edd0117845f6ecb69eafc9b06b7

No changes to commit

components/ClientProviders.tsx
@@ -3,18 +3,15 @@
 import ErrorBoundary from '@/components/ErrorBoundary'
 import { AgentInterceptorProvider } from '@/components/AgentInterceptorProvider'
 import { BrandProfileProvider } from '@/components/BrandProfileProvider'
-import { SSOGuard } from '@/components/SSOGuard'
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
     <ErrorBoundary>
-      <SSOGuard>
-        <AgentInterceptorProvider>
-          <BrandProfileProvider>
-            {children}
-          </BrandProfileProvider>
-        </AgentInterceptorProvider>
-      </SSOGuard>
+      <AgentInterceptorProvider>
+        <BrandProfileProvider>
+          {children}
+        </BrandProfileProvider>
+      </AgentInterceptorProvider>
     </ErrorBoundary>
   )
 }

commit 13ccab86e9bba6374cb4735cc68eac6df6c45141

Restore SSOGuard wrapper in ClientProviders (regressed by 4d114d4 Architect snapshot)

components/ClientProviders.tsx
@@ -3,15 +3,18 @@
 import ErrorBoundary from '@/components/ErrorBoundary'
 import { AgentInterceptorProvider } from '@/components/AgentInterceptorProvider'
 import { BrandProfileProvider } from '@/components/BrandProfileProvider'
+import { SSOGuard } from '@/components/SSOGuard'
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
     <ErrorBoundary>
-      <AgentInterceptorProvider>
-        <BrandProfileProvider>
-          {children}
-        </BrandProfileProvider>
-      </AgentInterceptorProvider>
+      <SSOGuard>
+        <AgentInterceptorProvider>
+          <BrandProfileProvider>
+            {children}
+          </BrandProfileProvider>
+        </AgentInterceptorProvider>
+      </SSOGuard>
     </ErrorBoundary>
   )
 }